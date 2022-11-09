6987c9e57fd222b3a7aa46fd259d715d33904b9e
Ana Julia Gari  Albano <anajulia.garialbano15@gmail.com>
2022-11-09 14:58:20 -0300

    Primeiro Desafio JS

--- java script/01 - variaveis e operadores/primeiro_desafio.js
+++ java script/01 - variaveis e operadores/primeiro_desafio.js
@@ -6,4 +6,10 @@ Você terá 3 variáveis. Sendo elas:
 3 - Distância em KM da viagem 
 
 */
-console.log('olá')
+const precoCombustivel = 5.79;
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+const valorGasto = litrosConsumidos * precoCombustivel;
+console.log(valorGasto.toFixed(2))
