30970422294223570a798e2f23aa594814095ff9
Ana Julia Gari  Albano <anajulia.garialbano15@gmail.com>
2022-11-10 14:36:17 -0300

    Estrutura apenas com if

--- java script/02 - estruturas condicionais/aula.js
+++ java script/02 - estruturas condicionais/aula.js
@@ -1,8 +1,16 @@
 
 
-const numero = 10;
+const numero = 2;
 
-const numeroPar = numero % 2;
-
+const numeroPar = (numero % 2) === 0;
 console.log(numeroPar);
+
+if (numeroPar) {
+    console.log('Par')
+}
+
+if (!numeroPar) {
+    console.log('Impar')
+}
+
 
